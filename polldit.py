92f376e0f322fba03836f02bc7af4a30d1acb0d5
nflanders9 <flanders.nick@gmail.com>
2015-10-21 00:28:08 -0400

    Refactor Subreddit class to get posts and comments separately; Add method to return a list of all text in a time interval along with its vote counts

--- polldit.py
+++ polldit.py
@@ -54,15 +54,17 @@ class Subreddit:
         self.name = name
         self.subreddit = REDDIT.get_subreddit(name[3:])
 
-    def comments(self, begin=None, end=None):
+
+    def posts(self, begin=None, end=None):
         """
-        Return a list of all comments on posts made between the given
-        datetime objects. If no begin or end dates are given, it defaults
-        to the last 24 hours
+        Return a list of all posts made between the given datetime objects.
+        If no begin or end dates are given, it defaults to the last 24 hours
 
         args:
-            begin: the early time bound for comments
-            end:   the late time bound for comments
+            begin: the early time bound for posts
+            end:   the late time bound for posts
+        return:
+            list of Submission objects
         """
         # set default parameters if none were passed to the method
         if end == None:
@@ -76,11 +78,50 @@ class Subreddit:
         if debug: print("Subreddit: {}\nNumber of posts in range: {}".format(
             self.name, len(posts)))
 
+        return posts
+
+
+    def comments_on_posts(self, post_list):
+        """
+        Return a single list of all the comments on all posts in the
+        given list of Submissions
+
+        args:
+            post_list:  list of Submission objects
+        return:
+            list of Comment objects
+        """
+
         comments = []
-        for submission in posts:
+        for submission in post_list:
             comments += list(praw.helpers.flatten_tree(submission.comments))
 
         if debug: print("Number of comments: {}".format(len(comments)))
         
         return comments
 
+
+    def get_text(self, begin=None, end=None):
+        """
+        Return a list of all text including comments and submissions that
+        were posted to this subreddit in the given time interval
+
+        args:
+            begin:  the datetime object of the beginning of the interval
+            end:    the datetime object of the end of the interval
+        return:
+            list of tuples (<text>, <num_upvotes>, <num_downvotes>)
+        """
+        # set default parameters if none were passed to the method
+        if end == None:
+            end = datetime.datetime.now()
+        if begin == None:
+            begin = end - datetime.timedelta(hours=1)
+
+        posts = self.posts(begin, end)
+        comments = self.comments_on_posts(posts)
+
+        posts_votes = [(post.title, post.ups, post.downs) for post in posts]
+        comments_votes = [(comment.body, comment.ups, comment.downs) for comment in comments]
+
+        return posts_votes + comments_votes
