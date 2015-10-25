d86f6456825dc47b8070468ac22d9b537c096f98
nflanders9 <flanders.nick@gmail.com>
2015-10-24 21:56:33 -0400

    Add class for storing text along with associated vote counts; Add method on Subreddits to return all recent comments to a given limit (1000 max due to API restrictions)

--- polldit.py
+++ polldit.py
@@ -36,11 +36,31 @@ REDDIT = praw.Reddit(USER_AGENT)
 SUBREDDITS = config['subreddits']
 REDDIT_URL = config['reddit_url']
 
+# initialize CANDIDATES to a dictionary of list of candidate names where the
+# keys are the last names of each candidate
+CANDIDATE_LIST = config['candidates']
+CANDIDATES = dict()
+for name in CANDIDATE_LIST:
+    CANDIDATES[name] = config['candidates'][name]
+
 
 # PRAW always returns a ResourceWarning as Python garbage collection
 # deletes the connection pool before processing it can be closed in 3.X
 if not debug:
     warnings.filterwarnings("ignore")
+
+
+class Text:
+    """
+    Represents text (a comment or a submission) with vote counts to reddit
+    """
+    def __init__(self, content, ups, downs):
+        self.content = content
+        self.ups = ups
+        self.downs = downs
+
+    def __str__(self):
+        return self.content
 
 
 class Subreddit:
@@ -93,8 +113,14 @@ class Subreddit:
         """
 
         comments = []
-        for submission in post_list:
+        for index, submission in enumerate(post_list):
+            if debug: print("Flattening: ", index)
+            # returns a list of MoreComments objects that were not replaced
+            remaining_more_comments = submission.replace_more_comments(limit=None)
+            while len(remaining_more_comments) > 0:
+                remaining_more_comments = submission.replace_more_comments()
             comments += list(praw.helpers.flatten_tree(submission.comments))
+            print(len(comments))
 
         if debug: print("Number of comments: {}".format(len(comments)))
         
@@ -110,7 +136,7 @@ class Subreddit:
             begin:  the datetime object of the beginning of the interval
             end:    the datetime object of the end of the interval
         return:
-            list of tuples (<text>, <num_upvotes>, <num_downvotes>)
+            list of Posts
         """
         # set default parameters if none were passed to the method
         if end == None:
@@ -121,7 +147,53 @@ class Subreddit:
         posts = self.posts(begin, end)
         comments = self.comments_on_posts(posts)
 
-        posts_votes = [(post.title, post.ups, post.downs) for post in posts]
-        comments_votes = [(comment.body, comment.ups, comment.downs) for comment in comments]
+        posts_votes = [Text(post.title, post.ups, post.downs) for post in posts]
+        comments_votes = [Text(comment.body, comment.ups, comment.downs) for comment in comments]
 
         return posts_votes + comments_votes
+
+
+    def recent_comments(self, limit=None):
+        """
+        Return a list of recent comments from this subreddit since the given
+        datetime object
+
+        args:
+            limit: the number of comment to return (up to 1000)
+        return:
+            list of Posts
+        """
+        comment_gen = self.subreddit.get_comments(limit=limit)
+        comments = praw.helpers.flatten_tree(comment_gen)
+
+        return [Post(comment.body, comment.ups, comment.downs) for comment in comments]
+
+
+def filter_by_candidate(post_list, candidate):
+    """
+    Returns a list of posts from the given list that mention the name of
+    the given candidate
+
+    args:
+        comment_list:   list of posts to filter
+        candidate:      name of candidate to filter for
+    return:
+        list of comments mentioning the given candidate
+    """
+    names = CANDIDATES[candidate]
+    output = []
+    for post in post_list:
+        for name in names:
+            if name in post.content:
+                output.append(post)
+    return output
+
+
+
+#android = Subreddit("politics")
+#android.recent_comments()
+
+politics = Subreddit("politics")
+text = politics.get_text()
+for post in filter_by_candidate(text, 'clinton'):
+    print("************", post, "\n\n")
